ea605655aba5faf5b58258e87231f75ece0329d8
马文卿 <529837505@qq.com>
2023-09-15 16:13:03 +0800

    feat: add shared module

---
+++ src/shared/system.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common'
+
+@Injectable()
+export class SystemService {
+
+    constructor() {
+    }
+
+    getEnv() {
+        return {
+            a: 1
+        }
+    }
+}
