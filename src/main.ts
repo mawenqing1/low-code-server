bb0240fd350b108fed08a8dcb75b87004177e49a
马文卿 <529837505@qq.com>
2023-09-15 14:55:03 +0800

    feat: add user module/swagger doc

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { generateDocument } from './doc'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  generateDocument(app);
+
+  await app.listen(8100);
 }
 bootstrap();
